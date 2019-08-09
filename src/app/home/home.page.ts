aa7d4158bd243f44c2a0304ee29b399f6b3180bd
Jukka-Pekka Keisala <jp@jpkeisala.com>
2019-08-09 08:53:14 +0200

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Word} from '../models/word';
+import { core } from '@angular/compiler';
+import {Storage} from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements OnInit {
+  data: any;
+  totalWords: number = 1000;
+  words: Array<Word> = [];
+  keyWord: Array<Word> = [];
 
-  constructor() {}
+  answerIshidden = true;
 
+
+  constructor(private storage: Storage) { }
+
+  ngOnInit() {
+    this.storage.get("data").then(d=>
+		{
+			if(d)
+			{
+        console.log('using local storage');
+        //debugger;
+				this.data = d;
+        this.getRandom();
+			}
+			else
+			{
+        fetch('./assets/data.json').then(res => res.json())
+        .then(json => {
+          this.storage.set('data', json); 
+          this.data = json;
+          this.getRandom();
+        });
+      }
+    });
+
+
+/*
+    if(this.storage.get("namssse")){
+      
+       
+      //this.data = this.storage.get("data");
+      
+      
+    }
+    else{
+      fetch('./assets/data.json').then(res => res.json())
+      .then(json => {
+        this.storage.set('data', json); 
+        this.data = json;
+        this.getRandom();
+      });
+    }
+    
+    */
+    
+
+  }
+
+  getRandom() {
+    this.totalWords = this.data.length;
+    this.answerIshidden = true;
+    this.words = [];
+    this.keyWord = [];
+    this.words.push(this.randomNumber());
+    this.words.push(this.randomNumber());
+    this.words.push(this.randomNumber());
+    this.words.push(this.randomNumber());
+    let randomKeyIndex = Math.floor(Math.random() * this.words.length)
+    this.keyWord.push(this.words[randomKeyIndex]);
+    //debugger;
+  }
+
+  randomNumber() {
+    let randomIndex = Math.floor(Math.random() * this.data.length)
+    return this.data[randomIndex]
+  }
+
+  checkAnswer(e) {
+    //debugger;
+
+    
+    console.log(e.target.id);
+    let correctId = this.keyWord[0].Number;
+    if(e.target.id === correctId){
+      if(this.answerIshidden != false){
+        this.data.splice(correctId, 1);
+        this.storage.set('data', this.data); 
+      }
+      
+      this.getRandom();
+      
+    }
+    else{
+      this.answerIshidden = false;
+    }
+    
+  }
 }
---
+++ src/app/models/word.ts
@@ -0,0 +1,5 @@
+export class Word {
+    Number: number; 
+    Danish: string;
+    English: string; 
+}
